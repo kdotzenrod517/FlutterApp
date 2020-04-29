39698ced75da5d5a7e0c07b7cb8be1c8fdb65ee9
Krista Dotzenrod <kdotzenrod517@gmail.com>
2020-04-28 22:25:16 -0500

    Initial Commit

---
+++ android/app/src/main/kotlin/com/kdotz/flutter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kdotz.flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('My First App'),
+        centerTitle: true,
+        backgroundColor: Colors.purple[400],
+      ),
+      body: Center(
+          child: Text(
+            'Hello Ninjas!',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey[600],
+              fontFamily: 'IndieFlower',
+            ),
+          )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('Click'),
+        backgroundColor: Colors.indigo[400],
+      ),
+    ),
+  ));
+}
+

212700eaa7f45b240bb575ae352db35c6424013c
Krista Dotzenrod <kdotzenrod517@gmail.com>
2020-05-05 21:59:54 -0500

    Playing with images

--- lib/main.dart
+++ lib/main.dart
@@ -2,30 +2,30 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
+    home: Home(),
+  ));
+}
+
+// Custom Stateless Widget
+// Widgets are classes
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text('My First App'),
         centerTitle: true,
         backgroundColor: Colors.purple[400],
       ),
-      body: Center(
-          child: Text(
-            'Hello Ninjas!',
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.grey[600],
-              fontFamily: 'IndieFlower',
-            ),
-          )
+      body: Padding(
+        padding: EdgeInsets.all(90.0),
+        child: Text('hello'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Text('Click'),
         backgroundColor: Colors.indigo[400],
       ),
-    ),
-  ));
+    );
+  }
 }
-
